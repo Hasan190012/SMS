e221152be2cb2430d821cfa97c69332764e2dace
Hassan Foyan <hassangnt18@gmail.com>
2025-04-28 14:10:21 +0300

    name was changed

--- applications/view/sidebar.php
+++ applications/view/sidebar.php
@@ -7,7 +7,7 @@
                 <div class="sidebar-brand-icon rotate-n-0">
                 <i class="bi bi-book-fill"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Modern Schools</div>
+                <div class="sidebar-brand-text mx-3">Modern Schools system</div>
             </a>
 
             <!-- Divider -->
